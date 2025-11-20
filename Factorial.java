56aa13df83e32a0c11563c95192c84d44c5a9eb7
huzaifa07-hub <5503549.faheem@gmail.com>
2025-11-20 08:55:08 +0530

    add Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
